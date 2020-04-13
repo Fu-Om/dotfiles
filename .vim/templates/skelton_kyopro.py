227c0cda6b4ace821f4de1be53b3f138915edbbe
fuuuumin <fuuuumin314@gmail.com>
2020-04-13 09:01:36 +0900

    Add template file.

---
+++ .vim/templates/skelton_kyopro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+from collections import Counter
+from itertools import permutations, combinations
+from fractions import gcd
+from math import ceil, floor
+sys.setrecursionlimit(10 ** 6)
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+def main():
+    <++> 
+
+if __name__ == '__main__':
+    main()

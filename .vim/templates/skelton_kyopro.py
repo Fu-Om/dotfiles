638b916f465b325e81ef64636497e325b8da14e0
fuuuumin <fuuuumin314@gmail.com>
2020-04-15 15:28:07 +0900

    Changed gcd from math to fractions.

--- .vim/templates/skelton_kyopro.py
+++ .vim/templates/skelton_kyopro.py
@@ -2,7 +2,8 @@
 import sys
 from collections import Counter
 from itertools import permutations, combinations
-from math import ceil, floor, gcd
+from fractions import gcd
+from math import ceil, floor
 sys.setrecursionlimit(10 ** 6)
 
 def input():
